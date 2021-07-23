29e9cef355d75220cd596d195cda6c218ba25c2b
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-07-23 05:42:01 +0100

    test: rewards setting

--- test/001_erc20_rewards.ts
+++ test/001_erc20_rewards.ts
@@ -1,6 +1,6 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
 
-import { constants } from '@yield-protocol/utils-v2'
+import { constants, id } from '@yield-protocol/utils-v2'
 const { WAD, MAX256 } = constants
 const MAX = MAX256
 
@@ -34,9 +34,22 @@ describe('ERC20Rewards', async function () {
   beforeEach(async () => {
     governance = (await deployContract(ownerAcc, ERC20MockArtifact, ["Governance Token", "GOV", 18])) as ERC20Mock
     rewards = (await deployContract(ownerAcc, ERC20RewardsArtifact, ["Token with rewards", "REW", 18])) as ERC20Rewards
+
+    await rewards.grantRoles(
+      [id('setRewards(address,uint192,uint32,uint32)')],
+      owner
+    )
   })
 
   it('sets a rewards token and schedule', async () => {
+    expect(await rewards.setRewards(governance.address, 1, 2, 3))
+    .to.emit(rewards, 'RewardsSet')
+    .withArgs(governance.address, 1, 2, 3)
+    const schedule = await rewards.schedule()
+    expect(await rewards.reward()).to.equal(governance.address)
+    expect(schedule.rate).to.equal(1)
+    expect(schedule.start).to.equal(2)
+    expect(schedule.end).to.equal(3)
   })
 
   describe('with a rewards schedule', async () => {
