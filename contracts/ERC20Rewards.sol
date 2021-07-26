682adb20e0415af80ea333076b45c633cd866af9
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-07-26 11:48:13 +0100

    tweak: Use Anton's algorithm. Faithfully.

--- contracts/ERC20Rewards.sol
+++ contracts/ERC20Rewards.sol
@@ -7,14 +7,6 @@ import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
 import "@yield-protocol/utils-v2/contracts/utils/RevertMsgExtractor.sol";
 
 
-library CastU256U224 {
-    /// @dev Safely cast an uint256 to an uint224
-    function u224(uint256 x) internal pure returns (uint224 y) {
-        require (x <= type(uint32).max, "Cast overflow");
-        y = uint224(x);
-    }
-}
-
 library CastU256U32 {
     /// @dev Safely cast an uint256 to an uint32
     function u32(uint256 x) internal pure returns (uint32 y) {
@@ -28,37 +20,31 @@ library CastU256U32 {
 /// by the size of their holdings.
 contract ERC20Rewards is AccessControl, ERC20Permit {
     using CastU256U32 for uint256;
-    using CastU256U224 for uint256;
 
-    event RewardsSet(IERC20 rewardToken, uint32 start, uint32 end, uint256 rate);
-    event Claimable(address user, uint256 claimable);
-    event Claimed(address user, uint256 claimed);
+    event RewardsSet(IERC20 rewardsToken, uint32 start, uint32 end, uint256 rate);
+    event RewardsPerToken(uint256 rewardsPerToken);
+    event UserRewards(address user, uint256 userRewards, uint256 rewardsPerTokenStored);
+    event Claimed(address receiver, uint256 claimed);
 
     struct RewardPeriod {
-        uint32 start;                                   // Start time for the current rewardToken schedule
-        uint32 end;                                     // End time for the current rewardToken schedule
+        uint32 start;                                   // Start time for the current rewardsToken schedule
+        uint32 end;                                     // End time for the current rewardsToken schedule
     }
 
-    struct SupplyTracker {
-        uint32 lastUpdated;                             // Last time the total supply average was computed
-        uint224 average;                                // Average total supply
-    }
+    IERC20 public rewardsToken;                         // Token used as rewards
+    RewardPeriod public rewardsPeriod;                  // Period in which rewards are accumulated by users
+    uint256 public rewardsRate;                         // Wei rewarded per second
 
-    struct UserRewards {
-        uint32 lastUpdated;                             // Last time the user rewards were updated
-        uint224 accumulated;                            // Rewards accumulated before the last update
-    }
+    uint256 public rewardsPerTokenStored;               // Accumulated rewards per token for the period
+    uint32 public lastUpdated;                          // Last time the rewards per token accumulator was updated
 
-    IERC20 public rewardToken;                          // Token used as rewards
-    RewardPeriod public rewardPeriod;                   // Period in which rewards are accumulated by users
-    SupplyTracker public supplyTracker;                 // Total supply average
-    uint256 public rewardRate;                          // Wei rewarded per second
-
-    mapping (address => UserRewards) public rewards;    // Rewards accumulated by users
+    mapping (address => uint256) public rewards;        // Rewards accumulated by users
+    mapping (address => uint256) public paidRewardPerToken;    // Last rewards per token level accumulated by each user
     
     constructor(string memory name, string memory symbol, uint8 decimals)
         ERC20Permit(name, symbol, decimals)
     { }
+
 
     /// @dev Submit a series of calls for execution
     /// @notice Allows batched call to self (this contract).
@@ -72,112 +58,92 @@ contract ERC20Rewards is AccessControl, ERC20Permit {
         }
     }
 
+    /// @dev Return the earliest of two timestamps
+    function earliest(uint32 x, uint32 y) internal pure returns (uint32 z) {
+        z = (x < y) ? x : y;
+    }
+
+    /// @dev Return the latest of two timestamps
+    function latest(uint32 x, uint32 y) internal pure returns (uint32 z) {
+        z = (x > y) ? x : y;
+    }
+
     /// @dev Set a rewards schedule
     /// TODO: Allow sequential schedules, each with their own supply accumulator and rewards accumulator. Only once active schdule at a time.
-    function setRewards(IERC20 rewardToken_, uint32 start, uint32 end, uint256 rate)
+    function setRewards(IERC20 rewardsToken_, uint32 start, uint32 end, uint256 rate)
         public
         auth
     {
-        if (rewardToken_ != IERC20(address(0))) rewardToken = rewardToken_; // TODO: Allow to change only after a safety period after end, to avoid affecting current claimable
+        if (rewardsToken_ != IERC20(address(0))) rewardsToken = rewardsToken_; // TODO: Allow to change only after a safety period after end, to avoid affecting current claimable
 
-        RewardPeriod memory rewardPeriod_ = rewardPeriod;
-        rewardPeriod_.start = start;
-        rewardPeriod_.end = end;
-        rewardPeriod = rewardPeriod_;
+        RewardPeriod memory rewardsPeriod_ = rewardsPeriod;
+        rewardsPeriod_.start = start;
+        rewardsPeriod_.end = end;
+        rewardsPeriod = rewardsPeriod_;
 
-        rewardRate = rate;
-        emit RewardsSet(rewardToken, start, end, rate);
+        rewardsRate = rate;
+        emit RewardsSet(rewardsToken, start, end, rate);
     }
 
-    /// @dev Claim all rewards from caller into a given address
-    function claim(address to)
-        public
-        returns (uint256 claiming)
-    {
-        UserRewards memory userRewards_ = rewards[msg.sender];
-        RewardPeriod memory rewardPeriod_ = rewardPeriod;
-        
-        // Calculate the period for which rewards haven't been recorded into the accumulator
-        uint32 start = (userRewards_.lastUpdated > rewardPeriod_.start) ? userRewards_.lastUpdated : rewardPeriod_.start;
-        uint32 end = (block.timestamp.u32() > rewardPeriod_.end) ? rewardPeriod_.end : block.timestamp.u32();
-        uint32 unaccountedPeriod = (end > start) ? end - start : 0;
+    /// @dev Accumulate rewards for an user and update the rewards per token accumulator.
+    /// @notice Needs to be called on each liquidity event
+    function updateRewardsPerToken() internal returns (uint256 rewardsPerToken) {
+        uint32 start = latest(lastUpdated, rewardsPeriod.start);
+        uint32 end = earliest(block.timestamp.u32(), rewardsPeriod.end);
+        require (end > start, "Rewards schedule complete");
+        uint32 timeSinceLastUpdated = end - start;
 
-        // Calculate the claimable amount
-        uint256 rewardsPerTokenPerSecond = rewardRate / supplyTracker.average;
-        claiming = unaccountedPeriod * _balanceOf[msg.sender] * rewardsPerTokenPerSecond + userRewards_.accumulated;
-
-        // Reset the user rewards records
-        userRewards_.accumulated = 0;
-        userRewards_.lastUpdated = block.timestamp.u32();
-        rewards[msg.sender] = userRewards_;
-
-        // Transfer out the rewards
-        rewardToken.transfer(to, claiming);
-        emit Claimed(to, claiming);
+        rewardsPerToken = rewardsPerTokenStored + timeSinceLastUpdated * rewardsRate / _totalSupply;
+        rewardsPerTokenStored = rewardsPerToken;
+        lastUpdated = earliest(block.timestamp.u32(), rewardsPeriod.end);
+        emit RewardsPerToken(rewardsPerToken);
     }
 
-    /// @dev Update the average supply as the time-weighted average between the previous average until the previous update,
-    /// and the current supply level from the previous update until now.
-    function _updateAverageSupply()
-        internal
-        returns (uint224 _averageSupply)
-    {
-        SupplyTracker memory supplyTracker_ = supplyTracker;
-        uint32 storedEnd = (supplyTracker_.lastUpdated > rewardPeriod.start) ? supplyTracker_.lastUpdated : rewardPeriod.start;
-        uint32 storedPeriod = storedEnd - rewardPeriod.start;
-        uint32 currentEnd = (block.timestamp.u32() > rewardPeriod.end) ? rewardPeriod.end : block.timestamp.u32();
-        uint32 currentPeriod = currentEnd - block.timestamp.u32();
-
-        if (currentPeriod == 0) return 0; // Save gas
-
-        supplyTracker_.average = _averageSupply = ((supplyTracker_.average * storedPeriod + _totalSupply * currentPeriod) / (storedPeriod + currentPeriod)).u224();
-        supplyTracker_.lastUpdated = block.timestamp.u32();
-        supplyTracker = supplyTracker_;
-        // emit
+    /// @dev Accumulate rewards for an user and update the rewards per token accumulator.
+    /// @notice Needs to be called on each liquidity event
+    function updateUserRewards(address user) internal returns (uint256 userRewards) {
+        uint256 rewardsPerTokenStored_ = rewardsPerTokenStored;
+        userRewards = rewards[user] + _balanceOf[user] * (rewardsPerTokenStored_ - paidRewardPerToken[user]);
+        rewards[user] = userRewards;
+        paidRewardPerToken[user] = rewardsPerTokenStored_;
+        emit UserRewards(user, userRewards, rewardsPerTokenStored_);
     }
 
-    function _updateUserRewards(address user)
-        internal
-        returns (uint224 _rewards)
-    {
-        UserRewards memory userRewards_ = rewards[user];
-        uint32 start = (userRewards_.lastUpdated > rewardPeriod.start) ? userRewards_.lastUpdated : rewardPeriod.start;
-        uint32 end = (block.timestamp.u32() > rewardPeriod.end) ? rewardPeriod.end : block.timestamp.u32();
-        uint32 unaccountedPeriod = (end > start) ? end - start : 0;
-
-        if (unaccountedPeriod == 0) return 0; // Save gas
-
-        uint256 rewardsPerTokenPerSecond = rewardRate / supplyTracker.average;
-        userRewards_.accumulated = _rewards = (unaccountedPeriod * _balanceOf[user] * rewardsPerTokenPerSecond + userRewards_.accumulated).u224();
-        userRewards_.lastUpdated = block.timestamp.u32();
-        rewards[user] = userRewards_;
-        // emit
-    }
-
-    /// @dev Mint tokens, updating the supply average before.
+    /// @dev Mint tokens, after accumulating rewards for an user and update the rewards per token accumulator.
     function _mint(address dst, uint256 wad)
         internal virtual override
         returns (bool)
     {
-        _updateUserRewards(msg.sender);
-        _updateAverageSupply();
+        updateRewardsPerToken();
+        updateUserRewards(msg.sender);
         return super._mint(dst, wad);
     }
 
-    /// @dev Burn tokens, updating the supply average before.
+    /// @dev Burn tokens, after accumulating rewards for an user and update the rewards per token accumulator.
     function _burn(address src, uint256 wad)
         internal virtual override
         returns (bool)
     {
-        _updateUserRewards(msg.sender);
-        _updateAverageSupply();
+        updateRewardsPerToken();
+        updateUserRewards(msg.sender);
         return super._burn(src, wad);
     }
 
-    /// @dev Transfer tokens, updating the accumulated rewards of the receiver to avoid double-counting.
+    /// @dev Transfer tokens, after accumulating rewards for an user and update the rewards per token accumulator.
     /// @notice The sender should batch the transfer with a `claim` before, to avoid losing rewards.
     function _transfer(address src, address dst, uint wad) internal virtual override returns (bool) {
-        _updateUserRewards(dst);
+        updateUserRewards(dst);
         return super._transfer(src, dst, wad);
+    }
+
+    /// @dev Claim all rewards from caller into a given address
+    function claim(address to)
+        external
+        returns (uint256 claiming)
+    {
+        claiming = updateUserRewards(msg.sender);
+        rewardsToken.transfer(to, claiming);
+        delete rewards[msg.sender];
+        emit Claimed(to, claiming);
     }
 }
