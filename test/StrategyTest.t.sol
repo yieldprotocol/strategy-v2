9a8251c52337753ea43e49c5caf2e5de375b05cd
alcueca <alberto@yield.is>
2022-11-08 21:52:22 +0000

    missing state transition

--- test/StrategyTest.t.sol
+++ test/StrategyTest.t.sol
@@ -4,7 +4,7 @@ import "forge-std/Test.sol";
 import "../contracts/Strategy.sol";
 
 // Deployed
-//   mint(4)
+//   mint(4) -> init -> Divested
 //   init -> Divested
 // Divested
 //   invest -> Invested
